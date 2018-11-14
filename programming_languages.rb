33a92f9b8628f1e20c63e080efcc5fb4e5b45944
John Mark Redding <johnmarkredding@gmail.com>
2018-11-14 17:30:02 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,7 @@ def reformat_languages(old_hash)
   old_hash.each do |style, langs|
     langs.each do |name, info|
       new_hash[name] = info
-      new_hash[name][:style] = style
+      new_hash[name][:style] = [style]
     end
   end
   new_hash
